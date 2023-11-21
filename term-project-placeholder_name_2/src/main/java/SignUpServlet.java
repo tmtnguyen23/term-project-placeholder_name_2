68a561e84e41c52862b65541f4ee1ece1d327cc3
thing <tmtnguyen@unomaha.edu>
2023-11-21 12:22:44 -0600

    Some adjustments to the library in SignUpServlet page.

--- term-project-placeholder_name_2/src/main/java/SignUpServlet.java
+++ term-project-placeholder_name_2/src/main/java/SignUpServlet.java
@@ -1,19 +1,12 @@
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
 /**
  * Servlet implementation class LoginServlet
  */
